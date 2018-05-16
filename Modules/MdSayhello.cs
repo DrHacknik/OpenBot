21dd5b63976d5d2b50aabfb923e37a181452b614
Dr.Hacknik <zoltx23@gmail.com>
2018-05-16 12:47:41 -0400

    Cleaned Modules and Fixed an issue with MdSayHello

--- Modules/MdSayhello.cs
+++ Modules/MdSayhello.cs
@@ -11,14 +11,14 @@ namespace RyuBot.Modules
 
         [Command("SayHello")]
         [RequireUserPermission(GuildPermission.ManageMessages)]
-        public async Task Say_Hello()
+        public async Task Say_Hello(SocketGuildUser MentionedUser)
 
         {
             var ReturnMsg = await Context.Channel.GetMessagesAsync(1).Flatten();
             await Context.Channel.DeleteMessagesAsync(ReturnMsg);
             await Context.Channel.SendMessageAsync("**I did not do it, I did not hit her; I did nawt!**");
             string Message = "Command **!sayhello** requested by <@" + Context.Message.Author.Id  + Environment.NewLine +
-                "in channel <#" + Context.Channel.Id + ">" + "Mentioned User: <" + user.Mention + ">";
+                "in channel <#" + Context.Channel.Id + ">" + "Mentioned User: <" + MentionedUser.Mention + ">";
 
             await Helper.LoggingAsync(new LogMessage(LogSeverity.Verbose, "Module", Message));
         }

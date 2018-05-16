a51c69a2184b680a9a4ea72676e9f4bad045ed2b
Dr.Hacknik <zoltx23@gmail.com>
2018-05-16 07:34:51 -0400

    Working on Modules

--- Modules/MdAbout.cs
+++ Modules/MdAbout.cs
@@ -20,17 +20,17 @@ namespace RyuBot
         {
             var messages = await this.Context.Channel.GetMessagesAsync(1).Flatten();
             await this.Context.Channel.DeleteMessagesAsync(messages);
-            await Context.Channel.SendMessageAsync(
-                "=====================================" +
-                "\r\nDr.Hackniks RyujiNX Bot for Discord" +
-                "\r\nVersion: 0.1.5" +
-                "\r\nBot name: OpenBot" +
-                "\r\nBot revision: 18_4_19_1034pm" +
-                "\r\nBot Type: " + key_stat + " | Web-socket-based" +
-                "\r\n=====================================");
-            Console.BackgroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine(time + ":: Command Request: !about; " + "ID <" + Context.User.Id.ToString() + "> " +
-                "\r\nUsername: <" + Context.User.Username.ToString() + ">" + " Channel ID: <" + Context.Channel.Id + ">");
-        }
+             string Message = "=====================================" + Environment.NewLine +
+                                 "RyuBot for Discord" + Environment.NewLine +
+                                 "by Dr.Hacknik & Ac_K" + Environment.NewLine +
+                                 "Version: 0.2.0" + Environment.NewLine +
+                                 "Bot name: RyuBot" + Environment.NewLine +
+                                 "Bot revision: 18_5_16_0000am" + Environment.NewLine +
+                                 "Bot Type: DotNet Core | Web-socket-based" + Environment.NewLine +
+                                 "=====================================";
+
+
+                await Helper.LoggingAsync(new LogMessage(LogSeverity.Verbose, "Bot", Message));
+                await Context.Channel.SendMessageAsync(Message);
     }
 }

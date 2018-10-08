ccc30a8d1701d7ac0bfe56eaa8ff6b48a8c4a770
Dr.Hacknik <zoltx23@gmail.com>
2018-10-08 16:53:37 -0400

    Cleaned up the Module
    * Fixed the API shit
    * Redid the way messages are counted and then flatten asynchronously.

--- Modules/MdDeleteMessages.cs
+++ Modules/MdDeleteMessages.cs
@@ -2,35 +2,34 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 
-namespace OpenBot.Modules {
-    public class MdDeleteMessages : ModuleBase<SocketCommandContext> {
-        [Command ("Delete-beta")]
-        [RequireUserPermission (GuildPermission.ManageMessages)]
-        public async Task DeleteMessages (int Number, ulong ChannelId = 0, [Remainder] string Reason = "Unknown") {
-            await Context.Channel.SendMessageAsync ("This command is currently not functional. Due to the new Discord.NET API.");
-            return;
+namespace OpenBot.Modules
+{
+    public class MdDeleteMessages : ModuleBase<SocketCommandContext>
+    {
+        [Command("Delete-beta")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task DeleteMessages(int Number, [Remainder] string Reason = null)
+        {
+            var Messages = ((SocketTextChannel)Context.Channel).GetMessagesAsync(Number + 1).FlattenAsync();
 
-            if (ChannelId == 0) {
-                return;
-            } else {
-                //await ((SocketTextChannel)Context.Channel).DeleteAsync();
-            }
+            await ((SocketTextChannel)Context.Channel).DeleteMessagesAsync(Messages.Result, null);
 
-            await Context.Message.DeleteAsync ();
+            await Context.Message.DeleteAsync();
 
-            string Message = "Command **!delete " + Number.ToString () + "** requested by " + Context.User.Username + ">" + Environment.NewLine +
-                "in channel <#" + Context.Channel.Id + "> for channel <#" + ((ChannelId == 0) ? Context.Channel.Id.ToString () : ChannelId.ToString ()) + ">" + Environment.NewLine +
+            string Message = "Command **!delete " + Number.ToString() + "** requested by " + Context.User.Username + ">" + Environment.NewLine +
+                "in channel <#" + Context.Channel.Id + ">" + Environment.NewLine +
                 "with reason: *" + Reason + "*";
 
-            await Helper.LoggingAsync (new LogMessage (LogSeverity.Verbose, "Module", Message));
+            await Helper.LoggingAsync(new LogMessage(LogSeverity.Verbose, "Module", Message));
 
             const int Delay = 5000;
-            var LastMsg = await Context.Channel.SendMessageAsync ($"Delete completed. _This message will be deleted in {Delay / 1000} seconds._");
+            var LastMsg = await Context.Channel.SendMessageAsync($"Delete completed. _This message will be deleted in {Delay / 1000} seconds._");
 
-            await Task.Delay (Delay);
+            await Task.Delay(Delay);
 
-            await LastMsg.DeleteAsync ();
+            await LastMsg.DeleteAsync();
         }
     }
 }

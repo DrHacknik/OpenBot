2907e2b95c39e0d2abf1c0c646dd9c8a33ff681d
Dr.Hacknik <zoltx23@gmail.com>
2018-10-08 16:54:48 -0400

    Cleaned Config

--- Config.cs
+++ Config.cs
@@ -11,10 +11,8 @@ namespace OpenBot
         public static string LewdDir;
         public static string BotDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\OpenBot"; //Could be "%appdata%/BotName/" if on Windows or "/home/username/.config/BotName/" if on Linux.
         public static string BotName = "OpenBot-beta";
-        public static string Version = "0.2.5.2";
-        public static string VersionSlim = "0_2_5_2";
-        public static string BuildDate = "9/30/2018: 4:08pm EST";
-        public static string BuildDateSlim = "9_30_2018_4_08_pm_EST";
+        public static string Version = "0.2.5.3";
+        public static string BuildDate = "10/8/2018: 4:53pm EST";
         public static string OS = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
         public static bool LogWithoutStamp;
     }
